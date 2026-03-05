96414cf9e480524d2550bed7eaa5a24bc781c912
huytu132 <huytugod@gmail.com>
2026-03-05 11:25:12 +0700

    fix

--- src/main/java/org/example/githubactions/GreetingController.java
+++ src/main/java/org/example/githubactions/GreetingController.java
@@ -1,11 +1,14 @@
 package org.example.githubactions;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/greet")
 public class GreetingController {
+
+    @GetMapping
     public String greet() {
         return "Hello, World!";
     }
