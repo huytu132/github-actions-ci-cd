f238dfceffa273d1a63f8308083db84bbf2c74ed
huytu132 <huytugod@gmail.com>
2026-03-05 14:00:55 +0700

    add

--- src/main/java/org/example/githubactions/GreetingController.java
+++ src/main/java/org/example/githubactions/GreetingController.java
@@ -1,8 +1,11 @@
 package org.example.githubactions;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api")
@@ -16,5 +19,11 @@ public class GreetingController {
     @GetMapping
     public String defaultEndpoint() {
         return "Welcome to the API!";
+    }
+
+    @GetMapping("/animals")
+    public ResponseEntity<List<String>> getAnimals() {
+        List<String> animals = List.of("Dog", "Cat", "Elephant", "Lion");
+        return ResponseEntity.ok(animals);
     }
 }
